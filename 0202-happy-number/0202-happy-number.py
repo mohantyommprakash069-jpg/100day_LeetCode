9af20d3e53f87e574c88ee0ee964fcddfd9a676d
Omm Prakash Mohanty <mohantyommprakash069@gmail.com>
2026-09-09 18:20:40 +0530

    Time: 1 ms (61.15%), Space: 12.3 MB (89.43%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,27 @@
+class Solution(object):
+
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        seen = set()
+
+        while n != 1:
+
+            if n in seen:
+                return False
+
+            seen.add(n)
+
+            total = 0
+
+            while n > 0:
+                r = n % 10
+                total += r * r
+                n //= 10
+
+            n = total
+
+        return True

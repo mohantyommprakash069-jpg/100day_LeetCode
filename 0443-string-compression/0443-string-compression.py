50bae48ca6d03348ea7db2a2f63146e1e7f16e1b
Omm Prakash Mohanty <mohantyommprakash069@gmail.com>
2026-09-17 19:35:52 +0530

    Time: 3 ms (60.5%), Space: 12.4 MB (55%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,24 +1,22 @@
 class Solution(object):
-
     def compress(self, chars):
-        left = 0
-        right = 0
-        n = len(chars)
+        write = 0
+        i = 0
 
-        while right < n:
-            ch = chars[right]
+        while i < len(chars):
+            ch = chars[i]
             count = 0
 
-            while right < n and chars[right] == ch:
+            while i < len(chars) and chars[i] == ch:
                 count += 1
-                right += 1
+                i += 1
 
-            chars[left] = ch
-            left += 1
+            chars[write] = ch
+            write += 1
 
             if count > 1:
                 for digit in str(count):
-                    chars[left] = digit
-                    left += 1
+                    chars[write] = digit
+                    write += 1
 
-        return left
+        return write

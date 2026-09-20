28cdf584e19e5efbccbeb57d787e6b28026ae3a1
Omm Prakash Mohanty <mohantyommprakash069@gmail.com>
2026-09-20 18:39:35 +0530

    Time: 4 ms (49.92%), Space: 14.7 MB (5.52%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -7,11 +7,22 @@ class Solution(object):
         # for i in range(len(nums)):
         #     if nums.count(i) == 1:
         #         return i
+        # for i in range(len(nums)):
+        #     count = 0
+        #     for j in range(len(nums)):
+        #         if nums[i] == nums[j]:
+        #             count+=1
+        #     if count == 1:
+        #         return nums[i]
+        #         break
+        dic={}
         for i in range(len(nums)):
-            count = 0
-            for j in range(len(nums)):
-                if nums[i] == nums[j]:
-                    count+=1
-            if count == 1:
-                return nums[i]
-                break
+            if nums[i] in dic:
+                dic[nums[i]]+=1
+            else:
+                dic[nums[i]]=1
+
+        for key in dic:
+
+            if dic[key] == 1:
+                return key

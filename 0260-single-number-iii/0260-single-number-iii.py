f95dd5fc2ccdd2a56deb90e87b5e3ad07c92d6f6
Omm Prakash Mohanty <mohantyommprakash069@gmail.com>
2026-09-24 17:55:35 +0530

    Time: 6 ms (24.88%), Space: 14.6 MB (12.06%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,19 +1,30 @@
 class Solution(object):
     def singleNumber(self, nums):
-        ans = 0
+        # ans = 0
 
-        for i in nums:
-            ans ^= i
+        # for i in nums:
+        #     ans ^= i
 
-        diff = ans & -ans
+        # diff = ans & -ans
 
-        a = 0
-        b = 0
+        # a = 0
+        # b = 0
 
-        for i in nums:
-            if i & diff:
-                a ^= i
+        # for i in nums:
+        #     if i & diff:
+        #         a ^= i
+        #     else:
+        #         b ^= i
+
+        # return a, b
+        dic = {}
+        for i in range(len(nums)):
+            if nums[i]  in dic:
+                dic[nums[i]]+=1
             else:
-                b ^= i
-
-        return a, b
+                dic[nums[i]] = 1
+        ans = []
+        for i in dic:
+            if dic[i] == 1:
+                ans.append(i)
+        return ans
